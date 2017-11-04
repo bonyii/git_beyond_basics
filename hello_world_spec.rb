5700af40d9567e511ae51082b705b3ea13689f7a
Bonaventura Fleischmann <dev+boni@a2-access.com>
2017-11-04 19:52:53 +0100

    Reorganize specs

--- hello_world_spec.rb
+++ hello_world_spec.rb
@@ -1,9 +1,11 @@
 require './hello_world'
 
 describe HelloWorld do
-  subject { described_class.new.print }
+  describe '#print' do
+    subject { described_class.new.print }
 
-  it "should print message" do
-    expect(subject).to eq('Hello World!')
+    it "should print message" do
+      expect(subject).to eq('Hello World!')
+    end
   end
 end
